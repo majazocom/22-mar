52dd8a3a5fef06c2d46acf383473c352b24d72db
majazocom <maja.gedda@zocom.se>
2021-03-22 09:59:29 +0100

    path test

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const port = 8080;
+const path = require('path');
+
+
+console.log(path.normalize('//s/e./rr//../c/'));
 
 http.createServer( (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Listening on port 8080');
     res.end('Hello World!');
-}).listen(8080);
+}).listen(port);
 
